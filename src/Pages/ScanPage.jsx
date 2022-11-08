131fef27838a6c2a712e5bda4e291c0604b4384d
Rounak00 <mukherjeerounak10@gmail.com>
2022-11-08 11:15:01 +0530

    git push

--- src/Pages/ScanPage.jsx
+++ src/Pages/ScanPage.jsx
@@ -93,7 +93,7 @@ const ScanPage = (props) => {
                   }
                 }}
                 style={{ width: '100%' , height:"100%" }}
-                constraints={ {facingMode: 'rear'} }
+                constraints={ {facingMode: 'environment'} }
             />  
       </Camera>
       <Result>
